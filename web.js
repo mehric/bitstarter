f3ff55033196aaed10c126721178f589aa57b548
Mehran <mehran.net1@gmail.com>
2013-07-19 08:39:19 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+var content = fs.readFileSync('index.html', 'utf8');
+var buffer = new Buffer(content.length);
+len = buffer.write(content, 0);
+response.send(buffer.toString('utf8', 0, len));
+
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
